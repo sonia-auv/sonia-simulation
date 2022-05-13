0bb5352c286a59c5722bf032f7a1e0b3934905c0
Kam0ss <Kam0ss>
2022-05-13 10:55:45 -0400

    changed positioning method

--- Assets/auv_package/Scripts/PositionSubscriber.cs
+++ Assets/auv_package/Scripts/PositionSubscriber.cs
@@ -7,41 +7,72 @@ using EstimatedRosPos = RosMessageTypes.Nav.OdometryMsg;
 public class PositionSubscriber : MonoBehaviour
 {
     public GameObject auv;
-    public string trueStateTopicName = "/proc_simulation/true_states";
+    public GameObject origin;
+    private string startSimulationTopicName = "/proc_simulation/start_simulation";
+    private string trueStateTopicName = "/proc_simulation/true_states";
     private string estimatedTopicName = "/telemetry/auv_states";
     
-    private 
+    //private Vector3 initPose;
+    //private Quaternion initOrientation;
+    private bool simulationMode = false ;
 
     void Start()
     {
+        ROSConnection.GetOrCreateInstance().Subscribe<TrueRosPos>(startSimulationTopicName,StartSimulation);
         ROSConnection.GetOrCreateInstance().Subscribe<TrueRosPos>(trueStateTopicName,TruePositionChange);
         ROSConnection.GetOrCreateInstance().Subscribe<EstimatedRosPos>(estimatedTopicName,EstimatedPositionChange);
-
+        Vector3 pose = auv.transform.position;
+        pose.y = (float)0.0;
+        origin.transform.position = pose;
+        origin.transform.rotation = auv.transform.rotation;
 
     }   
 
-    void EstimatedPositionChange(EstimatedRosPos positionMessage)
+    void StartSimulation(TrueRosPos positionMessage)
     {
         // Get message Info
-        Vector3 msgPos = new Vector3((float)positionMessage.pose.pose.position.y, -(float)positionMessage.pose.pose.position.z, (float)positionMessage.pose.pose.position.x);
-        Quaternion msgRot = Quaternion.Euler(-(float)positionMessage.pose.pose.orientation.y, (float)positionMessage.pose.pose.orientation.z, -(float)positionMessage.pose.pose.orientation.x);
-        
-        msgRot.x = -msgRot.x;
-        msgRot.y = -msgRot.y;
-        msgRot.z = -msgRot.z;
-        
-        auv.transform.position = msgPos;
-        auv.transform.rotation = msgRot;
+        //initPose = new Vector3((float)positionMessage.position.y, -(float)positionMessage.position.z, (float)positionMessage.position.x);
+        //initOrientation = new Quaternion(-(float)positionMessage.orientation.y,(float)positionMessage.orientation.z,(float)positionMessage.orientation.x,(float)positionMessage.orientation.w);
+
+        simulationMode = true;
+        // Debug.Log(initPose);
+        // Debug.Log(initOrientation);
     }
 
     void TruePositionChange(TrueRosPos positionMessage)
     {
-        // Get message Info
-        Vector3 msgPos = new Vector3((float)positionMessage.position.y, -(float)positionMessage.position.z, (float)positionMessage.position.x);
-        Quaternion msgRot = new Quaternion(-(float)positionMessage.orientation.y,(float)positionMessage.orientation.z,(float)positionMessage.orientation.x,(float)positionMessage.orientation.w);
-        
-        auv.transform.position = msgPos;
-        auv.transform.rotation = msgRot;
+        if (simulationMode)
+        {
+            // Get message Info
+            Vector3 msgPos = new Vector3((float)positionMessage.position.y, -(float)positionMessage.position.z, (float)positionMessage.position.x);
+            Quaternion msgRot = new Quaternion(-(float)positionMessage.orientation.y,(float)positionMessage.orientation.z,(float)positionMessage.orientation.x,(float)positionMessage.orientation.w);
+            
+            auv.transform.position = msgPos;
+            // msgRot.x = -msgRot.x;
+            // msgRot.y = -msgRot.y;
+            // msgRot.z = -msgRot.z;
+            auv.transform.rotation = msgRot;
+        }
+    }
+
+    void EstimatedPositionChange(EstimatedRosPos positionMessage)
+    {
+        if (!simulationMode)
+        {
+            // Get message Info
+            Vector3 msgPos = new Vector3((float)origin.transform.position.x + (float)positionMessage.pose.pose.position.y, -(float)positionMessage.pose.pose.position.z, (float)origin.transform.position.z + (float)positionMessage.pose.pose.position.x);
+            Quaternion msgRot = Quaternion.Euler(-(float)positionMessage.pose.pose.orientation.y, (float)positionMessage.pose.pose.orientation.z, -(float)positionMessage.pose.pose.orientation.x);
+            
+            // msgRot.x = -msgRot.x;
+            // msgRot.y = -msgRot.y;
+            // msgRot.z = -msgRot.z;
+            
+            //auv.transform.position = initPose + msgPos;
+            //auv.transform.rotation = initOrientation * msgRot;
+
+            auv.transform.position = msgPos;
+            auv.transform.rotation = origin.transform.rotation * msgRot;
+        }
     }
          
 }
--- Assets/auv_package/Scripts/TrajectorySubscriber.cs
+++ Assets/auv_package/Scripts/TrajectorySubscriber.cs
@@ -2,9 +2,8 @@ using System.Threading;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using MultiTrajectoryPoses = RosMessageTypes.Trajectory.MultiDOFJointTrajectoryPointMsg;
-using TrajectoryReset = RosMessageTypes.Std.BoolMsg;
 using SingleWaypoint = RosMessageTypes.SoniaCommon.AddPoseMsg;
-using InitialPose = RosMessageTypes.Geometry.PoseMsg; 
+//using InitialPose = RosMessageTypes.Geometry.PoseMsg; 
 using Point = RosMessageTypes.Geometry.PointMsg;
 using Orient = RosMessageTypes.Geometry.QuaternionMsg;
 
@@ -14,94 +13,93 @@ public class TrajectorySubscriber : MonoBehaviour
     public GameObject singleTarget;
     public GameObject auv;
     private string trajectoryTopicName = "/proc_planner/send_trajectory_list";
-    private string resetTopicName = "/proc_planner/reset_trajectory";
     private string singleTopicName = "/proc_control/add_pose";
-    private string initialConditionTopicName = "/proc_simulation/start_simulation";
-    private InitialPose initialPose;
+    //private string initialConditionTopicName = "/proc_simulation/start_simulation";
+    //private InitialPose initialPose;
     
 
     void Start()
     {
         ROSConnection.GetOrCreateInstance().Subscribe<MultiTrajectoryPoses>(trajectoryTopicName,MultiTrajectory);
-        ROSConnection.GetOrCreateInstance().Subscribe<TrajectoryReset>(resetTopicName,ResetTrajectory);
         ROSConnection.GetOrCreateInstance().Subscribe<SingleWaypoint>(singleTopicName,SingleTrajectory);
-        ROSConnection.GetOrCreateInstance().Subscribe<InitialPose>(initialConditionTopicName,GetInitialPose);
-        initialPose = new InitialPose(new Point(0,0,0),new Orient(0,0,0,0));
+        //initialPose = new InitialPose(new Point(0,0,0),new Orient(0,0,0,0));
 
     } 
 
-    void GetInitialPose(InitialPose pose)
-    {
-        initialPose = pose;
-    }
+    // void GetInitialPose(InitialPose pose)
+    // {
+    //     initialPose = pose;
+    // }
 
     void MultiTrajectory(MultiTrajectoryPoses trajectoryMessage)
     {
         // Get message Info
         int length = trajectoryMessage.transforms.Length;
 
-        for (int i = 0; i <= length; i = i+10) 
+        for (int i = 0; i < length; i = i+10) 
         {
-            //Vector3 msgPos = new Vector3((float)initialPose.position.y + (float)trajectoryMessage.transforms[i].translation.y, -(float)trajectoryMessage.transforms[i].translation.z, (float)initialPose.position.x + (float)trajectoryMessage.transforms[i].translation.x);
-            Vector3 msgPos = new Vector3((float)trajectoryMessage.transforms[i].translation.y, -(float)trajectoryMessage.transforms[i].translation.z, (float)trajectoryMessage.transforms[i].translation.x);
+            Vector3 msgPos = new Vector3((float)singleTarget.transform.position.x + (float)trajectoryMessage.transforms[i].translation.y, -(float)trajectoryMessage.transforms[i].translation.z, (float)singleTarget.transform.position.z + (float)trajectoryMessage.transforms[i].translation.x);
             Quaternion msgRot = new Quaternion(-(float)trajectoryMessage.transforms[i].rotation.y, (float)trajectoryMessage.transforms[i].rotation.z, (float)trajectoryMessage.transforms[i].rotation.x, (float)trajectoryMessage.transforms[i].rotation.w);
 
-            GameObject targetClone = Instantiate(singleTarget, msgPos, msgRot);
+            msgRot.x = -msgRot.x;
+            msgRot.y = -msgRot.y;
+            msgRot.z = -msgRot.z;
 
-            Debug.Log(msgPos);
+            // Rotation ne fonctionne pas bien
+            GameObject targetClone = Instantiate(singleTarget, msgPos, singleTarget.transform.rotation * msgRot);
 
             Destroy(targetClone, 5+i/10);
+
         }
-        
     }
 
     void SingleTrajectory(SingleWaypoint waypoint)
     {
 
-        Vector3 msgPos = new Vector3((float)waypoint.position.y, -(float)waypoint.position.z, (float)waypoint.position.x);
+        Vector3 msgPos = new Vector3((float)0, -(float)0, (float)0);
         Quaternion msgRot = Quaternion.Euler(-(float)waypoint.orientation.y, (float)waypoint.orientation.z, -(float)waypoint.orientation.x);
 
         int frame = waypoint.frame;
+        int time = waypoint.speed;
 
         if(frame == 0)
         {
-            singleTarget.transform.position = msgPos;
-            singleTarget.transform.rotation = msgRot;
+            msgPos = new Vector3((float)singleTarget.transform.position.x + (float)waypoint.position.y, -(float)waypoint.position.z, (float)singleTarget.transform.position.z + (float)waypoint.position.x);
+            msgRot.x = -msgRot.x;
+            msgRot.y = -msgRot.y;
+            msgRot.z = -msgRot.z;
+            msgRot = singleTarget.transform.rotation * msgRot;
         }
         else if(frame == 1)
         {
-            singleTarget.transform.position = auv.transform.position + msgPos;
-            // msgRot.x = -msgRot.x;
-            // msgRot.y = -msgRot.y;
-            // msgRot.z = -msgRot.z;
-            singleTarget.transform.rotation = auv.transform.rotation * msgRot;
+            msgPos = new Vector3((float)auv.transform.position.x + (float)waypoint.position.y, (float)auv.transform.position.y - (float)waypoint.position.z, (float)auv.transform.position.z + (float)waypoint.position.x);
+            msgRot.x = -msgRot.x;
+            msgRot.y = -msgRot.y;
+            msgRot.z = -msgRot.z;
+            msgRot = auv.transform.rotation * msgRot;
         }
         else if(frame == 2)
         {
-            singleTarget.transform.position = auv.transform.position + msgPos;
-            singleTarget.transform.rotation = msgRot;
+            msgPos = new Vector3((float)auv.transform.position.x + (float)waypoint.position.y, (float)auv.transform.position.y - (float)waypoint.position.z, (float)auv.transform.position.z + (float)waypoint.position.x);
+            msgRot.x = -msgRot.x;
+            msgRot.y = -msgRot.y;
+            msgRot.z = -msgRot.z;
+            msgRot = singleTarget.transform.rotation * msgRot;
         }
         else if(frame == 3)
         {
-            singleTarget.transform.position = msgPos;
-            // msgRot.x = -msgRot.x;
-            // msgRot.y = -msgRot.y;
-            // msgRot.z = -msgRot.z;
-            singleTarget.transform.rotation = auv.transform.rotation * msgRot;
+            msgPos = new Vector3((float)singleTarget.transform.position.x + (float)waypoint.position.y, -(float)waypoint.position.z, (float)singleTarget.transform.position.z + (float)waypoint.position.x);
+            msgRot.x = -msgRot.x;
+            msgRot.y = -msgRot.y;
+            msgRot.z = -msgRot.z;
+            msgRot = auv.transform.rotation * msgRot;
         }
-    }
 
+        // Rotation ne fonctionne pas bien
+        GameObject targetClone = Instantiate(singleTarget, msgPos, msgRot);
 
-    void ResetTrajectory(TrajectoryReset trajectoryReset)
-    {
-        if(trajectoryReset.data)
-        {
-            TrajectoryDone();
-        }
-    }
-
-    void TrajectoryDone()
-    {
+        Destroy(targetClone, time + 2);
 
     }
+
 }
