47fd8584b171d56ebebe110b584044c29d78027f
FrancoisCR <francois.cote-raiche.1@ens.etsmtl.ca>
2022-05-16 18:04:14 -0400

    implementation sonar

--- Assets/auv_package/Scripts/SonarDataPublisher.cs
+++ Assets/auv_package/Scripts/SonarDataPublisher.cs
@@ -24,6 +24,9 @@ public class SonarDataPublisher : MonoBehaviour
     private float openningAngle = 90;
     private UInt32 nBins = 128;
     private float angleBetweenBins;
+    private float[] ranges;
+    private GameObject ray;
+    public float Resolution = 0.08f;
 
 
     void Start()
@@ -31,14 +34,19 @@ public class SonarDataPublisher : MonoBehaviour
         sequence = 0;
 
         // Get camera sample time
-        ts = (1000.0/sonarFrameRate);
+        ts = (100.0/sonarFrameRate);
 
         // Sonar angles
-        angleBetweenBins = openningAngle / nBins;
+        angleBetweenBins = openningAngle / (float)nBins;
 
         // Initialize ros publisher
         ros = ROSConnection.GetOrCreateInstance();
         ros.RegisterPublisher<LaserScanMsg>(topicName);
+
+        // allocate range array
+        ranges = new float[nBins];
+
+        ray =new GameObject();
     }
     private void Update(){
         
@@ -62,7 +70,8 @@ public class SonarDataPublisher : MonoBehaviour
             dtk = dt; // Update new time
             sequence = sequence + 1 ;
 
-
+            GatherData();
+            Publish();
 
             // Publish(ref frontRenderer,ref compressedFrontTopicName);
 
@@ -71,17 +80,39 @@ public class SonarDataPublisher : MonoBehaviour
     private void GatherData()
     {
         UInt32 index = 0;
-        for(float angle = -openningAngle / 2 ; angle <= openningAngle / 2; angle+= angleBetweenBins)
+        for(float angle = -openningAngle / 2.0f ; angle < openningAngle / 2.0f; angle+= angleBetweenBins)
         {
             RaycastHit hit;
-            if(Sonar.Physics.Raycast(Sonar.transform.position,Sonar.transform.TransformDirection(Vector3.forward),out hit,20,layerMasker));
+            ray.transform.rotation = Sonar.transform.rotation;
+            ray.transform.Rotate(90-angle,-90,0); //ZXY convention
+            
+            if(Physics.Raycast(Sonar.transform.position,ray.transform.forward,out hit,20)) // transform forward -> axeZ(bleu)
+            {
+                Debug.DrawRay(Sonar.transform.position, ray.transform.forward * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                ranges[index] = hit.distance + UnityEngine.Random.Range(-Resolution/2.0f,Resolution/2.0f);
+            }
+            else
+            {
+                Debug.DrawRay(Sonar.transform.position, ray.transform.forward * 1000, Color.red,1);
+                Debug.Log("Did not Hit");
+                ranges[index] = 20 + UnityEngine.Random.Range(-Resolution/2.0f,Resolution/2.0f); // range max, mais avoir
+            }
+            ++index;
         }
-    }
+    }       
 
-    private void Publish(ref GameObject cam, ref string rawTopic, ref string compressedTopic)
+    private void Publish()
     {
-        HeaderMsg header = new HeaderMsg();
-        
-        ros.Send(rawTopic, cameraData);
-        Destroy(tex);
+        LaserScanMsg msg = new LaserScanMsg();
+        msg.header.seq = sequence;
+        msg.header.frame_id = "BODY";
+        msg.angle_min = -openningAngle/2 *3.141592f /180.0f;
+        msg.angle_max = openningAngle/2 *3.141592f /180.0f;
+        msg.angle_increment = angleBetweenBins *3.141592f /180.0f;
+        msg.range_min = 0.20f;
+        msg.range_max = 20.0f;
+        msg.ranges = ranges;
+        ros.Send(topicName, msg);
     }
+}
